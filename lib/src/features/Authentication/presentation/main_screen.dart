03171f57c6b28fdf57f07ae925801f947261ee10
Cihan1990 <cihan.52@hotmail.de>
2024-05-08 11:08:40 +0200

    main screen verbessert

--- lib/src/features/Authentication/presentation/main_screen.dart
+++ lib/src/features/Authentication/presentation/main_screen.dart
@@ -35,21 +35,74 @@ class _MainScreenState extends State<MainScreen> {
             height: 852,
             decoration: const BoxDecoration(color: Color(0xFFE6E9FF)),
             child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Form(
-                  child: Column(children: [
-                    const SizedBox(height: 0),
-                    Container(
-                      width: 217,
-                      height: 76,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('lib/assets/images/bayfinlogo.png'),
-                          fit: BoxFit.fill,
-                        ),
+              padding: const EdgeInsets.all(10),
+              child: Form(
+                child: Column(children: [
+                  const SizedBox(height: 0),
+                  Container(
+                    width: 217,
+                    height: 76,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('lib/assets/images/bayfinlogo.png'),
+                        fit: BoxFit.fill,
                       ),
                     ),
-                  ]),
-                ))));
+                  ),
+                  const SizedBox(
+                    width: 96,
+                    height: 20,
+                    child: Text(
+                      'Guten Tag ',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 113,
+                    height: 40,
+                    child: Text(
+                      'Cihan!',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 34,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  Container(
+                    width: 361,
+                    height: 80,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFF212C95),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text(
+                      'Girokonto\n1.890,69€',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            )));
   }
 }
