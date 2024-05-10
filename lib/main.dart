b73097244ac1993b0c49b888d14b2f3b92a85a0c
Cihan1990 <cihan.52@hotmail.de>
2024-05-10 10:51:06 +0200

    angepasst

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:bayfin/src/features/Authentication/presentation/home_screen.dart';
 import 'package:bayfin/src/features/Authentication/presentation/login_screen.dart';
-import 'package:bayfin/src/features/Authentication/presentation/passwort_return_accept_screen.dart';
-import 'package:bayfin/src/features/Authentication/presentation/passwort_return_screen.dart';
-import 'package:bayfin/src/features/Authentication/presentation/registration_screen.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
