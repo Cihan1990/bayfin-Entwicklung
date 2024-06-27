6b4539e5933652b7e6da1f5ce17f5a0702d238d3
Cihan1990 <cihan.52@hotmail.de>
2024-06-27 14:33:40 +0200

    anpassungen fertig

--- lib/src/features/Authentication/presentation/registration_screen.dart
+++ lib/src/features/Authentication/presentation/registration_screen.dart
@@ -14,7 +14,10 @@ class RegistrationScreen extends StatefulWidget {
   final DatabaseRepository databaseRepository;
   final AuthRepository authRepository;
   // Konstruktor
-  const RegistrationScreen({super.key, required this.databaseRepository, required this.authRepository});
+  const RegistrationScreen(
+      {super.key,
+      required this.databaseRepository,
+      required this.authRepository});
 
   @override
   State<RegistrationScreen> createState() => _RegistrationScreenState();
@@ -25,6 +28,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late TextEditingController nachnameController;
   late TextEditingController geburtsdatumController;
   late TextEditingController mailController;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -51,98 +56,105 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             child: Padding(
           padding: const EdgeInsets.all(10),
           child: Form(
+              key: _formKey,
               child: Column(children: [
-            const SizedBox(height: 55),
-            LogoWidget(width: 217, height: 76),
-            SizedBox(height: 10),
-            Text('Registrieren',
-                style: TextStyle(
-                    shadows: const [
-                      Shadow(color: Colors.white, offset: Offset(0, -5))
-                    ],
-                    color: Colors.transparent,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Color(0xFFFFFFFF),
-                    decorationThickness: 1.35)),
-            const SizedBox(height: 25),
-            Prounouns(text: '  Anrede'),
-            SizedBox(height: 5),
-            const SizedBox(height: 15),
-            RegistrationsText(
-              controller: vornameController,
-              text: '  Vorname',
-              validator: validateVn,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-            ),
-            SizedBox(height: 5),
-            const SizedBox(height: 10),
-            RegistrationsText(
-              controller: nachnameController,
-              text: '  Nachname',
-              validator: validateNn,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-            ),
-            SizedBox(height: 5),
-            const SizedBox(height: 10),
-            RegistrationsText(
-              controller: geburtsdatumController,
-              text: '  Geburtsdatum',
-              hinttext: 'TT.MM.JJJJ',
-              validator: validateGb,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-            ),
-            SizedBox(height: 5),
-            SizedBox(height: 10),
-            RegistrationsText(
-              controller: mailController,
-              validator: validateEmail,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              text: '  E-Mail Adresse',
-            ),
-            SizedBox(height: 5),
-            const SizedBox(height: 110),
-            ElevatedButton(
-              child: Text('Passwort erstellen'),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PasswortAddScreen(
-                      databaseRepository: widget.databaseRepository,
-                      authRepository: widget.authRepository,
-                      email:mailController.text
-                    ),
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginScreen(
-                            databaseRepository: widget.databaseRepository,
-                            authRepository: widget.authRepository,
-                          ),
-                        ));
-                  },
-                  child: Text("Zurück zur Anmeldung",
-                      style: TextStyle(
+                const SizedBox(height: 55),
+                LogoWidget(width: 217, height: 76),
+                SizedBox(height: 10),
+                Text('Registrieren',
+                    style: TextStyle(
                         shadows: const [
                           Shadow(color: Colors.white, offset: Offset(0, -5))
                         ],
                         color: Colors.transparent,
-                        fontSize: 16,
+                        fontSize: 18,
                         decoration: TextDecoration.underline,
-                        decorationColor: const Color(0xFFFFFFFF),
-                        decorationThickness: 1.35,
-                      ))),
-            ]),
-          ])),
+                        decorationColor: Color(0xFFFFFFFF),
+                        decorationThickness: 1.35)),
+                const SizedBox(height: 25),
+                Prounouns(text: '  Anrede'),
+                SizedBox(height: 5),
+                const SizedBox(height: 15),
+                RegistrationsText(
+                  controller: vornameController,
+                  text: '  Vorname',
+                  validator: validateVn,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                SizedBox(height: 5),
+                const SizedBox(height: 10),
+                RegistrationsText(
+                  controller: nachnameController,
+                  text: '  Nachname',
+                  validator: validateNn,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                SizedBox(height: 5),
+                const SizedBox(height: 10),
+                RegistrationsText(
+                  controller: geburtsdatumController,
+                  text: '  Geburtsdatum',
+                  hinttext: 'TT.MM.JJJJ',
+                  validator: validateGb,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                SizedBox(height: 5),
+                SizedBox(height: 10),
+                RegistrationsText(
+                  controller: mailController,
+                  validator: validateEmail,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  text: '  E-Mail Adresse',
+                ),
+                SizedBox(height: 5),
+                const SizedBox(height: 110),
+                ElevatedButton(
+                  child: Text('Passwort erstellen'),
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PasswortAddScreen(
+                              databaseRepository: widget.databaseRepository,
+                              authRepository: widget.authRepository,
+                              email: mailController.text),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(height: 20),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginScreen(
+                                    databaseRepository:
+                                        widget.databaseRepository,
+                                    authRepository: widget.authRepository,
+                                  ),
+                                ));
+                          },
+                          child: Text("Zurück zur Anmeldung",
+                              style: TextStyle(
+                                shadows: const [
+                                  Shadow(
+                                      color: Colors.white,
+                                      offset: Offset(0, -5))
+                                ],
+                                color: Colors.transparent,
+                                fontSize: 16,
+                                decoration: TextDecoration.underline,
+                                decorationColor: const Color(0xFFFFFFFF),
+                                decorationThickness: 1.35,
+                              ))),
+                    ]),
+              ])),
         )));
   }
 
