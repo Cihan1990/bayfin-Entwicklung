3a78ad596561fab962b85fe20d7c77f235759d6f
Cihan1990 <cihan.52@hotmail.de>
2024-08-14 14:20:35 +0200

    transaktionsliste verändert

---
+++ lib/src/features/Authentication/presentation/widget/transaction_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:bayfin/src/data/database_repository.dart';
+import 'package:bayfin/src/features/authentication/presentation/widget/transaction_info.dart';
+import 'package:bayfin/src/features/bank_balance/domain/umsatz.dart';
+
+class TransactionListWidget extends StatelessWidget {
+  final String userId;
+  final String kontoId;
+
+  const TransactionListWidget({
+    Key? key,
+    required this.userId,
+    required this.kontoId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<Umsatz>>(
+      stream: context.read<DatabaseRepository>().getUmsatz(userId, kontoId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Icon(Icons.error);
+        } else if (snapshot.hasData &&
+            snapshot.connectionState == ConnectionState.active) {
+          List<Umsatz> umsatzliste = snapshot.data!;
+          List<TransactionInfo> transactionliste = [];
+
+          for (var i = 0; i < umsatzliste.length && i < 3; i++) {
+            var u = umsatzliste[i];
+            transactionliste.add(TransactionInfo(
+              firmName: u.umsatzname,
+              type: u.type,
+              amount: u.betrag,
+            ));
+          }
+
+          return Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 180, 183, 249),
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+            ),
+            width: 361,
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 15),
+                ...transactionliste,
+              ],
+            ),
+          );
+        } else {
+          return const Text("No Data");
+        }
+      },
+    );
+  }
+}
--- lib/src/features/bank_balance/presentation/main_screen.dart
+++ lib/src/features/bank_balance/presentation/main_screen.dart
@@ -2,19 +2,16 @@ import 'package:bayfin/src/data/auth_repository.dart';
 import 'package:bayfin/src/data/database_repository.dart';
 import 'package:bayfin/src/features/authentication/domain/benutzer.dart';
 import 'package:bayfin/src/features/authentication/presentation/widget/logo_widget.dart';
-import 'package:bayfin/src/features/authentication/presentation/widget/transaction_info.dart';
+import 'package:bayfin/src/features/authentication/presentation/widget/transaction_list.dart';
 import 'package:bayfin/src/features/bank_balance/domain/kontoinformationen.dart';
-import 'package:bayfin/src/features/bank_balance/domain/umsatz.dart';
 import 'package:bayfin/src/features/bank_balance/presentation/sales_screen.dart';
 import 'package:bayfin/src/features/bank_balance/presentation/view_bankaccount.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
-  // Attribute
-
   final KontoInformation kontoInformation;
-  // Konstruktor
+
   const MainScreen({super.key, required this.kontoInformation});
 
   @override
@@ -32,12 +29,7 @@ class _MainScreenState extends State<MainScreen> {
     databaseRepository = context.read<DatabaseRepository>();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  Future<void> aktualisiereKontoInformation(userId) async {
+  Future<void> aktualisiereKontoInformation(String userId) async {
     final aktualisierteKontoInfo = await databaseRepository.getKontoInfo(
         userId, kontoInformation.documentReference!.id);
     if (aktualisierteKontoInfo != null) {
@@ -54,202 +46,145 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ViewBankaccount()),
-                );
-              },
-              icon: const Icon(
-                Icons.arrow_back,
-                size: 24,
-              ))),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const ViewBankaccount()),
+            );
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 24,
+          ),
+        ),
+      ),
       body: FutureBuilder(
-          future: context.read<DatabaseRepository>().loadUserData(userId),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return const Icon(Icons.error);
-            } else if (snapshot.hasData) {
-              final Benutzer user = snapshot.data!;
-              return SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Form(
-                    child: Column(
+        future: context.read<DatabaseRepository>().loadUserData(userId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Icon(Icons.error);
+          } else if (snapshot.hasData) {
+            final Benutzer user = snapshot.data!;
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 0),
+                    LogoWidget(width: 217, height: 76),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        const SizedBox(height: 0),
-                        LogoWidget(width: 217, height: 76),
-                        const SizedBox(height: 20),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const SizedBox(width: 10),
-                              SizedBox(
-                                width: 96,
-                                height: 20,
-                                child: Text(
-                                  'Guten Tag ',
-                                  style: Theme.of(context).textTheme.labelLarge,
-                                ),
-                              ),
-                            ]),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            const SizedBox(width: 6),
-                            SizedBox(
-                              width: 113,
-                              height: 40,
-                              child: Text(
-                                user.vorname,
-                                style:
-                                    Theme.of(context).textTheme.headlineMedium,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
+                        const SizedBox(width: 10),
                         SizedBox(
-                            width: 361,
-                            child: InkWell(
-                                onTap: () async {
-                                  bool shouldUpdate = await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => SalesScreen(
-                                        kontoInformation: kontoInformation,
-                                      ),
-                                    ),
-                                  );
-                                  if (shouldUpdate) {
-                                    await aktualisiereKontoInformation(userId);
-                                  }
-                                },
-                                child: Card(
-                                    color: const Color.fromARGB(
-                                        255, 180, 183, 249),
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(12.0),
-                                        child: StreamBuilder(
-                                            stream: context
-                                                .read<DatabaseRepository>()
-                                                .getKontoInformation(userId),
-                                            builder: (context, snapshot) {
-                                              if (snapshot.hasData &&
-                                                  snapshot.connectionState ==
-                                                      ConnectionState.active) {
-                                                return Center(
-                                                  child: Text("${snapshot.data!.first.kontostand.toString()} €"),
-                                                );
-                                              } else {
-                                                return const Text("No Data");
-                                              }
-                                            }))))),
-                        const SizedBox(height: 15),
-                        Container(
-                          width: 361,
-                          height: 226,
-                          decoration: ShapeDecoration(
-                            image: const DecorationImage(
-                              image: AssetImage('assets/images/aktienbild.png'),
-                              fit: BoxFit.fill,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
+                          width: 96,
+                          height: 20,
+                          child: Text(
+                            'Guten Tag ',
+                            style: Theme.of(context).textTheme.labelLarge,
                           ),
                         ),
-                        const SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            const SizedBox(width: 6),
-                            Text(
-                              'Letzte Umsätze',
-                              style: Theme.of(context).textTheme.headlineSmall,
-                            ),
-                          ],
-                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(width: 6),
                         SizedBox(
-                          width: 361,
-                          child: Center(
-                            child: StreamBuilder<List<Umsatz>>(
+                          width: 113,
+                          height: 40,
+                          child: Text(
+                            user.vorname,
+                            style: Theme.of(context).textTheme.headlineMedium,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: 361,
+                      child: InkWell(
+                        onTap: () async {
+                          bool shouldUpdate = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SalesScreen(
+                                  kontoInformation: kontoInformation),
+                            ),
+                          );
+                          if (shouldUpdate) {
+                            await aktualisiereKontoInformation(userId);
+                          }
+                        },
+                        child: Card(
+                          color: const Color.fromARGB(255, 180, 183, 249),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: StreamBuilder<List<KontoInformation>>(
                               stream: context
                                   .read<DatabaseRepository>()
-                                  .getUmsatz(
-                                      userId,
-                                      widget.kontoInformation.documentReference!
-                                          .id),
+                                  .getKontoInformation(userId),
                               builder: (context, snapshot) {
                                 if (snapshot.hasData &&
                                     snapshot.connectionState ==
                                         ConnectionState.active) {
-                                  List<Umsatz> umsatzliste = snapshot.data!;
-
-                                  List<TransactionInfo> transactionliste = [];
-                                  int i = 0;
-                                  for (var u in umsatzliste) {
-                                    if (i < 3) {
-                                      transactionliste.add(TransactionInfo(
-                                        firmName: u.umsatzname,
-                                        type: u.type,
-                                        amount: u.betrag,
-                                      ));
-                                      i++;
-                                    }
-                                  }
-                                  // FALL: Future ist komplett und hat Daten!
-                                  return Column(
-                                    children: [
-                                      Container(
-                                        decoration: const BoxDecoration(
-                                            color: Color.fromARGB(
-                                                255, 180, 183, 249),
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(30))),
-                                        width: 361,
-                                        padding: const EdgeInsets.only(
-                                            left: 20, right: 20, bottom: 30),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.stretch,
-                                          children: [
-                                            const SizedBox(height: 15),
-                                            ...transactionliste
-                                          ],
-                                        ),
-                                      ),
-                                    ],
+                                  return Center(
+                                    child: Text(
+                                      "${snapshot.data!.first.kontostand.toString()} €",
+                                    ),
                                   );
-                                } else if (snapshot.connectionState !=
-                                    ConnectionState.waiting) {
-                                  // FALL: Sind noch im Ladezustand
-                                  return const Center(
-                                      child: CircularProgressIndicator());
                                 } else {
-                                  // FALL: Es gab nen Fehler
-                                  return const Icon(Icons.error);
+                                  return const Text("No Data");
                                 }
                               },
                             ),
                           ),
                         ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      width: 361,
+                      height: 226,
+                      decoration: ShapeDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/aktienbild.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(width: 6),
+                        Text(
+                          'Letzte Umsätze',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
                       ],
                     ),
-                  ),
+                    TransactionListWidget(
+                      userId: userId,
+                      kontoId: widget.kontoInformation.documentReference!.id,
+                    ), // Use the new widget here
+                  ],
                 ),
-              );
-            } else {
-              return const Text('Keine Kontodaten gefunden.');
-            }
-          }),
+              ),
+            );
+          } else {
+            return const Text('Keine Kontodaten gefunden.');
+          }
+        },
+      ),
     );
   }
 }
